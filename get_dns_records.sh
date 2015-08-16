d0047783c9ab298a4e3fcf31ea43d3d029b7dcc6
jdlubrano <joel.lubrano@gmail.com>
2015-08-15 23:16:47 -0400

    Remove some files

--- get_dns_records.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Get zone information from the Cloudflare API
-
-curl -X GET "https://api.cloudflare.com/client/v4/zones/e212c00c1baee47a4e1612ebd05477d3/dns_records" \
--H "Content-Type:application/json" \
--H "X-Auth-Key:0bcaa0af019075127f76b143f9fb6467e1e34" \
--H "X-Auth-Email:joel.lubrano@gmail.com"
-
-echo ""
--- get_zone.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Get zone information from the Cloudflare API
-
-curl -X GET "https://api.cloudflare.com/client/v4/zones/?name=jdlubrano.work" \
--H "Content-Type:application/json" \
--H "X-Auth-Key:0bcaa0af019075127f76b143f9fb6467e1e34" \
--H "X-Auth-Email:joel.lubrano@gmail.com"
-
-echo ""
